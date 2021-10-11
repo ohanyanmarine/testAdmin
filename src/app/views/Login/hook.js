bfb52a869339477a5e68a5f480a6cd250bbd08e5
Marine <m.v.oanyan@gmail.com>
2021-10-11 22:06:45 +0400

    modify login page style

--- src/app/views/Login/hook.js
+++ src/app/views/Login/hook.js
@@ -6,11 +6,30 @@ import { loginAction } from "../../store/actions";
 
 export default () => {
   const { register, handleSubmit } = useForm();
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   const dispatch = useDispatch();
   const submit = (data) => {
     console.log(data);
     dispatch(loginAction(data));
   };
 
-  return { register, handleSubmit, submit };
+  return { register, handleSubmit, submit, handleChange, handleClickShowPassword, handleMouseDownPassword, values, setValues };
 };
--- src/app/views/Login/index.js
+++ src/app/views/Login/index.js
@@ -14,26 +14,14 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import hook from "./hook";
 
 export default function InputWithIcon() {
-  const { register, handleSubmit, submit } = hook();
-  const [values, setValues] = React.useState({
-    password: "",
-    showPassword: false
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+  const { register,
+    handleSubmit,
+    submit,
+    handleChange,
+    handleClickShowPassword,
+    handleMouseDownPassword,
+    values
+  } = hook();
 
   return (
     <div className="wrapper">
@@ -46,51 +34,50 @@ export default function InputWithIcon() {
         <div className="title">
           <h1>DOCTORS</h1>
         </div>
-        <form onSubmit={handleSubmit(submit)}>
-        
-          
-            <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-              <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-              <TextField
-                id="input-with-sx"
-                label="Username"
-                variant="standard"
-                {...register("username", {
+        <form className="form" onSubmit={handleSubmit(submit)}>
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+            <TextField
+              id="input-with-sx"
+              label="Username"
+              variant="standard"
+              {...register("username", {
+                required: true
+              })}
+            />
+          </Box>
+          <Box>
+            <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">
+                Password
+              </InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                {...register("password", {
                   required: true
                 })}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
               />
-            </Box>
-            <Box>
-              <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-                <InputLabel htmlFor="standard-adornment-password">
-                  Password
-                </InputLabel>
-                <Input
-                  id="standard-adornment-password"
-                  type={values.showPassword ? "text" : "password"}
-                  value={values.password}
-                  {...register("password", {
-                    required: true
-                  })}
-                  onChange={handleChange("password")}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {values.showPassword ? (
-                          <VisibilityOff />
-                        ) : (
-                          <Visibility />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </Box>
+            </FormControl>
+          </Box>
+          <Box sx={{mt: 5}}>
             <Button
               type="submit"
               variant="outlined"
@@ -98,8 +85,7 @@ export default function InputWithIcon() {
             >
               Sign in
             </Button>
-         
-        
+          </Box>
         </form>
       </div>
     </div>
